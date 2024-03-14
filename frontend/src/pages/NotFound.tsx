6ec6279f7362fb7d01064d806487db9f213e712e
ShixinTang <shixin.tang@outlook.com>
2024-03-13 21:53:43 -0400

    Improve NotFound page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { TypeAnimation } from "react-type-animation";
 
 const NotFound = () => {
-    return <div>NotFound</div>;
+    return (
+        <Box width={"100%"} height={"100%"}>
+            <Box
+                sx={{
+                    display: "flex",
+                    width: "100%",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    mx: "auto",
+                    mt: 3,
+                }}>
+                <Box>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed once, initially
+                            "Sorry, Page Not Found",
+                            3000,
+                            "404",
+                            3000,
+                            "Please go back to the home page",
+                            3000,
+                        ]}
+                        speed={50}
+                        style={{
+                            fontSize: "50px",
+                            color: "white",
+                            display: "inline-block",
+                            textShadow: "1px 1px 20px #000",
+                        }}
+                        repeat={Infinity}
+                    />
+                </Box>
+            </Box>
+        </Box>
+    );
 };
 
 export default NotFound;
